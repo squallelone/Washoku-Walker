adbc2176c55fb7cca0d55fefd1f3429a5b5f179d
Justin-K-Ellis <jlke45@gmail.com>
2025-04-30 15:16:14 +0900

    Moving heading styling from scoped css to index.css

--- client/src/pages/landing-page/LandingPage.tsx
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -4,14 +4,14 @@ import "./styles.css";
 export default function LandingPage({ setView }: SetViewPropsInterface) {
   return (
     <section>
-      <h1>What are you looking for?</h1>
+      <h1 className="page-title">What are you looking for?</h1>
       <div className="choices">
         <div className="card" onClick={() => setView("recommendations")}>
-          <h2>I know what I want to eat</h2>
+          <h2 className="sub-heading">I know what I want to eat</h2>
           <p>Show me restaurant recommendations for a specific dish.</p>
         </div>
         <div className="card" onClick={() => setView("userSelection")}>
-          <h2>I'm not sure where to start.</h2>
+          <h2 className="sub-heading">I'm not sure where to start.</h2>
           <p>Show restaurant recommendations near me.</p>
         </div>
       </div>
