6d36daaa0099dd02fa8e8a7484f2221040411a81
Justin-K-Ellis <jlke45@gmail.com>
2025-04-30 12:21:56 +0900

    Stylize layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 import Header from "./components/header/Header.tsx";
 
 export default function App() {
-  return <Header />;
+  return (
+    <div className="outer-container">
+      <Header />
+    </div>
+  );
 }
