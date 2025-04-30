4d2dfb441b7cf966fa49f2c66f817955450a0891
Justin-K-Ellis <jlke45@gmail.com>
2025-04-30 16:53:07 +0900

    Let user select from a dropdown menu of cusines types

--- client/src/pages/user-selection/UserSelection.tsx
+++ client/src/pages/user-selection/UserSelection.tsx
@@ -1,3 +1,36 @@
+import { useState, useEffect } from "react";
+
 export default function UserSelection() {
-  return <div>UserSelection</div>;
+  const [cuisineTypes, setCusinesType] = useState<string[] | null>(null);
+  const url = import.meta.env.VITE_CUISINE_TYPES;
+
+  useEffect(() => {
+    getCusineTypes();
+  }, []);
+
+  async function getCusineTypes() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCusinesType(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="page-title">Select a Food and Find a Restaurant</h1>
+      <div className="container">
+        <select>
+          <option value=""></option>
+          {cuisineTypes?.map((cuisine) => (
+            <option key={cuisine} value={cuisine}>
+              {cuisine}
+            </option>
+          ))}
+        </select>
+      </div>
+    </section>
+  );
 }
