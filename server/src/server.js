6c7c29193a1b9f447ce721cdf80e3a2ce67060fc
Justin-K-Ellis <jlke45@gmail.com>
2025-05-01 11:04:22 +0900

    Create src dir and move relivant files there.

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./router.js";
-// import nearbyRestaurantsRoutes from "./routes/nearByRestaurants.js";
-// import restaurantsByDishRoutes from "./routes/restaurantsByDish.js";
 
 dotenv.config();
 
@@ -14,11 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/test", (req, res) => {
-  res.json({ message: "testing" });
+  res.json({ ok: true, message: "testing" });
 });
 app.use("/api/restaurant", router);
-// app.use("/api/restaurants/nearby", nearbyRestaurantsRoutes);
-// app.use("/api/restaurants/by-dish", restaurantsByDishRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
