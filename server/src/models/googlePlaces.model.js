c7f7595afeff9dc469e61cb382d66178379697cf
Justin-K-Ellis <jlke45@gmail.com>
2025-05-01 15:02:40 +0900

    Refine return data from google places api call.

--- server/src/models/googlePlaces.model.js
+++ server/src/models/googlePlaces.model.js
@@ -4,18 +4,17 @@ dotenv.config();
 // This is for users who aren't sure what they want and need restaurant
 // recommendations near them.
 async function getRecommendedRestaurants(locationData) {
-  console.log(process.env.GOOGLE_MAPS_API_KEY);
-
   const url = "https://places.googleapis.com/v1/places:searchNearby";
   const response = await fetch(url, {
     method: "post",
     headers: {
       "Content-Type": "application/json",
       "X-Goog-Api-Key": process.env.GOOGLE_MAPS_API_KEY,
-      "X-Goog-FieldMask": "places.displayName",
+      "X-Goog-FieldMask":
+        "places.displayName,places.rating,places.formattedAddress,places.websiteUri,places.regularOpeningHours.openNow",
     },
     body: JSON.stringify({
-      includedTypes: ["restaurant"],
+      includedPrimaryTypes: ["restaurant"],
       maxResultCount: 5,
       locationRestriction: {
         circle: {
