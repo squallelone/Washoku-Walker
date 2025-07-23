58d409f0c4f27ea737fef41c3c6b21894763b620
Justin-K-Ellis <jlke45@gmail.com>
2025-07-23 15:24:32 +0900

    feat: add content to landing page cards

---
+++ client/src/components/LandingPageCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue'
+import { RouterLink } from 'vue-router'
+
+defineProps({
+  title: String,
+  content: String,
+  link: String,
+})
+</script>
+
+<template>
+  <div class="p-4 rounded-2xl shadow flex flex-col gap-4 items-center justify-center bg-base-200">
+    <h1 class="font-bold text-2xl">{{ title }}</h1>
+    <p>
+      {{ content }}
+    </p>
+    <button class="btn btn-accent">
+      <RouterLink :to="link">Go</RouterLink>
+    </button>
+  </div>
+</template>
