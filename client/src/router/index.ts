6e5bd5015987cc6d7761f383b202a357719d02ff
Justin-K-Ellis <jlke45@gmail.com>
2025-07-23 15:23:32 +0900

    feat: set up routes, dummy pages for search & nearby rests

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from '../views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import SearchView from '@/views/SearchView.vue'
+import NearbyView from '@/views/NearbyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,16 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: SearchView,
+    },
+    {
+      path: '/nearby',
+      name: 'nearby',
+      component: NearbyView,
     },
   ],
 })
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,21 @@
-<script setup></script>
+<script setup>
+import LandingPageCard from '@/components/LandingPageCard.vue'
+import text from '../assets/siteText.json'
+</script>
 
-<template>Home</template>
+<template>
+  <div class="flex flex-col gap-8 w-9/10 mx-auto mt-4">
+    <!-- Search -->
+    <LandingPageCard
+      :title="text.landingPage.searchCard.title"
+      :content="text.landingPage.searchCard.content"
+      :link="text.landingPage.searchCard.link"
+    />
+    <!-- Nearby -->
+    <LandingPageCard
+      :title="text.landingPage.nearbyCard.title"
+      :content="text.landingPage.nearbyCard.content"
+      :link="text.landingPage.nearbyCard.link"
+    />
+  </div>
+</template>
