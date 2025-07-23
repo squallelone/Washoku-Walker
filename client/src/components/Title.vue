1aff419251dcd851c4898ee6fd715d08cfaf02ec
Justin-K-Ellis <jlke45@gmail.com>
2025-07-23 15:57:18 +0900

    feat: create Title component

---
+++ client/src/components/Title.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  text: String,
+})
+</script>
+
+<template>
+  <h1 class="font-bold text-2xl md:text-4xl text-center mt-4 md:mt-8">{{ text }}</h1>
+</template>
