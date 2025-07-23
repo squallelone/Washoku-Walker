aea40f99e5de893c24689678bba5c57346c9f765
Justin-K-Ellis <jlke45@gmail.com>
2025-07-23 21:26:54 +0900

    feat: add content to dummy login page
    The button for Google login is here, but Firebase auth will have to be
    set up later (as will any other social auth).

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import GoogleLogin from '@/components/GoogleLogin.vue'
+import Title from '@/components/Title.vue'
+</script>
 
 <template>
-  <h1>Login</h1>
+  <div
+    class="flex flex-col justify-center items-center border border-base-300 p-4 my-4 mx-auto w-9/10 md:w-6/10 rounded shadow"
+  >
+    <Title text="Login" />
+    <GoogleLogin />
+    <div class="mb-4"></div>
+  </div>
 </template>
