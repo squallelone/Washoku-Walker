168833a5d306e0541e1b14570a5ae423520f4ae5
Justin-K-Ellis <jlke45@gmail.com>
2025-07-25 21:48:05 +0900

    feat: create loading & error message components

---
+++ client/src/components/ErrorMessage.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col justify-center items-center mt-8 gap-2 text-error">
+    <h2 class="font-bold text-3xl">Uh-oh.</h2>
+    <p class="text-center text-2xl">
+      We're sorry, it looks like something went wrong when trying to load this page.
+    </p>
+    <p class="tex-xl">Please try again later.</p>
+  </div>
+</template>
---
+++ client/src/components/LoadingSpinner.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex justify-center items-center h-full mt-32">
+    <span class="loading loading-dots loading-xl text-secondary"></span>
+  </div>
+</template>
