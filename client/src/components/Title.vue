6ce310c7290cddd55d8c1c2daa7330f46083ddd2
Justin-K-Ellis <jlke45@gmail.com>
2025-07-27 13:46:01 +0900

    fix: refactor Tile component to use TS

--- client/src/components/Title.vue
+++ client/src/components/Title.vue
@@ -1,10 +1,11 @@
-<script setup>
+<script setup lang="ts">
 import { defineProps } from 'vue'
-defineProps({
-  text: String,
-})
+
+const props = defineProps<{
+  text: string
+}>()
 </script>
 
 <template>
-  <h1 class="font-bold text-2xl md:text-4xl text-center my-4 md:my-8">{{ text }}</h1>
+  <h1 class="font-bold text-2xl md:text-4xl text-center my-4 md:my-8">{{ props.text }}</h1>
 </template>
