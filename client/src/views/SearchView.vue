c8aa4908ba53143d79dd4b97bbef36b5f75ebb56
Justin-K-Ellis <jlke45@gmail.com>
2025-07-30 09:03:46 +0900

    feat: let user toggle between dish and area input modes
    For both the dish selection and location selection, the user can now
    toggle between selecting from a list and entering a free-form dish or
    location.

--- client/src/views/SearchView.vue
+++ client/src/views/SearchView.vue
@@ -14,16 +14,18 @@ import text from '../assets/siteText.json'
 const api = import.meta.env.VITE_API_URL
 
 // === Selection Data ===
-const selectedWard = ref('...')
+const selectedLocation = ref('...')
 const selectedDish = ref('...')
 const searchDone = ref(false)
+const dishMode = ref<'traditional' | 'freeForm'>('traditional')
+const locationMode = ref<'tokyo' | 'freeForm'>('tokyo')
 const { loading, fetchError, restaurants, fetchRestaurants } = useFetchRestaurants()
 
 // === Form submission ===
 async function handleSubmit(event: Event): Promise<void> {
   event.preventDefault()
 
-  const url = `${api}/by-dish-area?dish=${selectedDish.value}&area=${selectedWard.value}`
+  const url = `${api}/by-dish-area?dish=${selectedDish.value}&area=${selectedLocation.value}`
   await fetchRestaurants(url)
   if (restaurants) {
     searchDone.value = true
@@ -33,6 +35,19 @@ async function handleSubmit(event: Event): Promise<void> {
 function resetSearch(): void {
   searchDone.value = false
   restaurants.value = []
+}
+
+// === Toggle functions ===
+function toggleDishMode() {
+  dishMode.value === 'traditional'
+    ? (dishMode.value = 'freeForm')
+    : (dishMode.value = 'traditional')
+}
+
+function toggleLocationMode() {
+  locationMode.value === 'tokyo'
+    ? (locationMode.value = 'freeForm')
+    : (locationMode.value = 'tokyo')
 }
 </script>
 
@@ -49,28 +64,47 @@ function resetSearch(): void {
       <div class="flex flex-row w-full gap-2 justify-center items-center">
         <p class="w-2/10 text-xl">Find</p>
         <!-- Dishes -->
-        <select class="select" v-model="selectedDish">
+        <select v-if="dishMode === 'traditional'" class="select" v-model="selectedDish">
           <option v-for="dish of text.dishes" :value="dish.name">{{ dish.name }}</option>
         </select>
+        <input v-else type="text" class="input" placeholder="Your dish" v-model="selectedDish" />
       </div>
       <div class="flex flex-row w-full gap-2 justify-center items-center">
         <p class="w-2/10 text-xl">in</p>
         <!-- Tokyo Wards -->
-        <select class="select" v-model="selectedWard">
+        <select v-if="locationMode === 'tokyo'" class="select" v-model="selectedLocation">
           <option v-for="ward of text.tokyoWards" :value="ward">
             {{ ward }}
           </option>
         </select>
+        <input v-else type="text" class="input" placeholder="A place" v-model="selectedLocation" />
       </div>
       <button type="submit" class="btn btn-primary w-5/10 mx-auto my-2">
         {{
-          selectedWard !== '...' || selectedDish !== '...'
-            ? `Find ${selectedDish} in ${selectedWard}`
+          selectedLocation !== '...' || selectedDish !== '...'
+            ? `Find ${selectedDish} in ${selectedLocation}`
             : 'Find'
         }}
       </button>
     </form>
-    <SearchToggles />
+
+    <!-- Search Toggles -->
+    <div class="collapse bg-base-100 border-base-300 border mt-4">
+      <input type="checkbox" />
+      <div class="collapse-title font-semibold md:text-center">Search Options</div>
+      <div class="collapse-content text-sm flex flex-col gap-2 md:flex-row md:justify-around">
+        <div class="flex flex-row gap-2">
+          <input type="checkbox" class="toggle" @click="toggleDishMode" />
+          <span v-if="dishMode === 'traditional'">Traditional Food</span>
+          <span v-else>Anything</span>
+        </div>
+        <div class="flex flex-row gap-2">
+          <input type="checkbox" class="toggle" @click="toggleLocationMode" />
+          <span v-if="locationMode === 'tokyo'">Tokyo Wards</span>
+          <span v-else>Anywhere</span>
+        </div>
+      </div>
+    </div>
   </div>
   <div v-else class="flex justify-center">
     <button class="btn btn-primary" @click="resetSearch">Search again</button>
