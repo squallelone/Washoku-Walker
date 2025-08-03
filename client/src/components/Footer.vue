ce227c9c06ad67daa44a568390660f423096fa7c
Justin-K-Ellis <jlke45@gmail.com>
2025-08-03 16:38:11 +0900

    feat: create basic footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
+
+<template>
+  <footer class="fixed bottom-0 left-0 w-full flex justify-center items-center bg-primary p-4">
+    <p class="font-light">&copy; Justin Klitgaard {{ currentYear }}</p>
+  </footer>
+</template>
