0acc3c47db82a84506ae920d200d02fe35545f74
Justin-K-Ellis <jlke45@gmail.com>
2025-08-03 16:46:28 +0900

    feat: add links to footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,7 +3,16 @@ const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <footer class="fixed bottom-0 left-0 w-full flex justify-center items-center bg-primary p-4">
-    <p class="font-light">&copy; Justin Klitgaard {{ currentYear }}</p>
+  <footer
+    class="fixed bottom-0 left-0 w-full flex flex-col justify-center items-center bg-primary p-4 md:p-8"
+  >
+    <p class="font-light">Justin Klitgaard &copy; {{ currentYear }}</p>
+    <span class="flex flex-row justify-between gap-2 font-light">
+      <a href="https://github.com/Justin-K-Ellis" class="underline">GitHub</a>
+      <div>|</div>
+      <a href="https://www.linkedin.com/in/justin-klitgaard-ellis-384b9965/" class="underline"
+        >LinkedIn</a
+      >
+    </span>
   </footer>
 </template>
