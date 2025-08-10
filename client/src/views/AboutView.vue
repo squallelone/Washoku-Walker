3d60c6d9b63e4ac0dea45d97e0a85ae921b29331
Justin-K-Ellis <jlke45@gmail.com>
2025-08-10 13:21:34 +0900

    fix: change div to secion (for semantic html)

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -5,7 +5,7 @@ import text from '../assets/siteText.json'
 
 <template>
   <Title text="About" />
-  <div class="flex flex-col gap-4">
+  <section class="flex flex-col gap-4">
     <p class="text-justify md:text-xl" v-for="para in text.aboutPage">{{ para }}</p>
-  </div>
+  </section>
 </template>
